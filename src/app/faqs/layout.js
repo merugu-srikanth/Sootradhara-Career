e342a05e92fd356c8380aa391ea8093150597bf6
merugu-srikanth <merugusrikanth28@gmail.com>
2026-04-01 16:12:23 +0530

    The dist added here

--- src/app/faqs/layout.js
+++ src/app/faqs/layout.js
@@ -1,8 +1,9 @@
-import FAQs from "./FAQsClient";
+import FAQs from "./page";
 
 export const metadata = {
   title: "FAQs - Career Mitra",
-  description: "Find answers to FAQs about govt job alerts, internships, and career support on Careermitra. Get quick help on notifications and updates.",
+  description:
+    "Find answers to FAQs about govt job alerts, internships, and career support on Careermitra. Get quick help on notifications and updates.",
   keywords: "career mitra faqs, govt job alerts faqs, internship faqs India",
 };
 
